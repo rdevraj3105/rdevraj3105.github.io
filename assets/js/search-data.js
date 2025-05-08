19c6a95c1dd23dbdf72d956f50edf0907a0aa25d
rdevraj3105 <33137272+rdevraj3105@users.noreply.github.com>
2025-05-08 15:49:55 +0000

    Deploying to gh-pages from @ rdevraj3105/rdevraj3105.github.io@a9eed5e7ca9e794329e88796430e6d637dd31930 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,7 +21,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/publications/";
+            window.location.href = "/projects/";
           },
         },{id: "nav-resume",
           title: "resume",
